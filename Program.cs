bfe14851d6a792b1618712e5598a4dae0a220623
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2026-05-20 15:13:58 +0530

    first commit- with literals

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using C_Practise.stringpractice;
+
+LiteralsDataTypes.PrintData();
---
+++ literalpractice/LiteralsDataTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Practise.stringpractice
+{
+    public class LiteralsDataTypes
+    {
+        public static void PrintData()
+        {
+           //Char data type
+            Console.WriteLine('b');
+
+            //Integer
+            Console.WriteLine(123);
+
+            //Float -> f ->literal suffix
+            Console.WriteLine(0.1125F);
+
+            //double
+            Console.WriteLine(0.25);
+
+            //decimal
+            Console.WriteLine(12.39816m);
+
+            //Boolean
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+        }
+    }
+}
