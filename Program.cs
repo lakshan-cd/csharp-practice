989b008d23a55bf1c31c98cd2804740c808a81b2
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2026-05-20 15:35:33 +0530

    Simple variables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿using C_Practise.stringpractice;
+using C_Practise.variables;
 
 LiteralsDataTypes.PrintData();
+Console.WriteLine("============================Varibles=======================");
+Variables.printVariables();
---
+++ variables/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Practise.variables
+{
+    internal class Variables
+    {
+        public static void printVariables()
+        {
+            char myChar;
+            int gameScore;
+            decimal price;
+            bool processedCustomer;
+
+
+            string firstName;
+            firstName = "Bob";
+
+            Console.WriteLine(firstName);
+
+            string lastName = "Marlie";
+            Console.WriteLine(lastName);
+        }
+    }
+}
