0809fa9315d384b16bddf5e4bce2db6d2f3b7615
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2026-05-20 18:04:19 +0530

    Implicitly variables

---
+++ variables/ImplicitlyVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Practise.variables
+{
+    internal class ImplicitlyVariables
+    {
+        private static void printImplicitlyVariables()
+        {
+            var message = "Hellow World!"; //have to implicitly asign value to the variable  , will act as a string variable because of the value assigned to it
+            Console.WriteLine(message);
+        }
+    }
+}
