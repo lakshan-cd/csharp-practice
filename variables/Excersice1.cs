5936ba467731c6a44a97918a390105fe7412727e
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2026-05-20 18:04:39 +0530

    Exercise one

---
+++ variables/Excersice1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Practise.variables
+{
+    internal class Excersice1
+    {
+        public static void printWord()
+        {
+            string name = "Bob";
+            int count = 3;
+            float temperature = 36.50f;
+
+            Console.WriteLine("Hello," + name + "!" + " You have " + count + " messages in your inbox. The temperature is " + temperature + " celsius.");
+        }
+    }
+}
